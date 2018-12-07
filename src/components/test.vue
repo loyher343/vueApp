7bc7becf6aeff579a2be5414b6376de7e6f62f6e
unknown <loyhernandez59@gmail.com>
2018-12-07 01:22:52 -0800

    added my project

---
+++ src/components/test.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="test">
+        <h1>{{title}}</h1>
+        <p>{{user.first_name}}</p>
+        <p v-text="user.last_name"></p>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        name: 'test',
+        data() {
+            return {
+                title: 'Hello World',
+                user: {
+                    first_name: 'Loy',
+                    last_name: 'Hernandez'
+                }
+                
+            }
+        }
+    }
+</script>
+
+<style scoped>
+</style>
