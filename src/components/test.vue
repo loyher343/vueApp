d996dddbdab14b0abcce9bc33a9c3c274a025a2c
unknown <loyhernandez59@gmail.com>
2018-12-07 01:40:12 -0800

    html parse

--- src/components/test.vue
+++ src/components/test.vue
@@ -3,6 +3,8 @@
         <h1>{{title}}</h1>
         <p>{{user.first_name}}</p>
         <p v-text="user.last_name"></p>
+        <span v-html="sonnet121"></span>
+       
     </div>
 </template>
 
@@ -13,6 +15,7 @@
         data() {
             return {
                 title: 'Hello World',
+                sonnet121: '<p>Tis better to be vile than vile esteemed When not to be receives reproach of being, And the just pleasure lost, wich is so deemed Not by our feeling but by others seeing. for why should others false adulterate eyes Give salutatuon to my sportive blood? Or on my frailties why are frailer spies, Which in their wills count bad that I think good? No, I am that I am; and they that level At my abuses reckon up their own: I may be straight though they themselves be bevel; by their rank thoughts m y deeds must not be shown, Unless this general evil they maintain: All men are bad and in their badness reign</p>',
                 user: {
                     first_name: 'Loy',
                     last_name: 'Hernandez'
